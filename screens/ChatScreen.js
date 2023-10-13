bba4710bb0fbfa83f8c733c24111d44cb21da180
Макарова Юлия Андреевна <julmakan@gmail.com>
2023-10-13 23:07:37 +0300

    Navigation 2

---
+++ screens/ChatScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export default function ChatScreen({ }) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
+            <Text>Chat</Text>
+        </View>
+    )
+}
